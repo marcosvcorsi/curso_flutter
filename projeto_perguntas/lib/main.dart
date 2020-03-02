ab6d7bbb051e573c86fe4a990a93ed3707ace316
Marcos Corsi <marcosvcorsi@gmail.com>
2020-03-01 22:59:46 -0300

    Initial commit

---
+++ projeto_perguntas/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(PerguntaApp());
+
+class PerguntaApp extends StatefulWidget {
+  @override
+  State<PerguntaApp> createState() => PerguntaAppState();
+}
+
+class PerguntaAppState extends State<PerguntaApp> {
+  var perguntaSelecionada = 0;
+
+  void responder() {
+    setState(() {
+      perguntaSelecionada++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?'
+    ];
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
